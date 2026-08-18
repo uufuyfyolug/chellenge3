6bf468b82a984c38dd5d55a698719add1a6c161d
Alexrandre Oliveira <greenduda1500@gmail.com>
2026-08-18 14:49:03 -0300

    fiz tema claro e escuro

--- screens/script.js
+++ screens/script.js
@@ -6,3 +6,32 @@ document.getElementById("foto").addEventListener("click", () => {
         origin: { y: 0.89 }  // Origem vertical (0 a 1)
     });
 });
+const botaoTema = document.getElementById("tema");
+const iconeTema = document.getElementById("iconeTema");
+
+
+if (localStorage.getItem("tema") === "claro") {
+    document.body.classList.add("claro");
+    iconeTema.src = "imagens/lua.png";
+} else {
+    document.body.classList.remove("claro");
+    iconeTema.src = "imagens/brilho-do-sol.png";
+}
+
+botaoTema.addEventListener("click", function () {
+
+    document.body.classList.toggle("claro");
+
+    if (document.body.classList.contains("claro")) {
+
+        localStorage.setItem("tema", "claro");
+        iconeTema.src = "imagens/lua.png";
+
+    } else {
+
+        localStorage.setItem("tema", "escuro");
+        iconeTema.src = "imagens/brilho-do-sol.png";
+
+    }
+
+});
